2fce4ced05541083d9d09c71e10ca5ab466ef937
chericode <heo2922@naver.com>
2024-07-03 13:31:34 +0900

    feat : 다빈님 merge 고려해서 작업 완료

--- management_app/urls.py
+++ management_app/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views.family_list_views import UserCareListView
 from .views import family_list_views, family_post_views, volunteer_list_views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -11,7 +10,6 @@ urlpatterns = [
     path("care/delete/<int:care_id>/", family_post_views.delete_care, name='delete_care'),
     path("add/senior/", family_post_views.add_senior),
     path("care/detail/<int:care_id>/", family_post_views.show_one_care),
-    path("my_cares/", UserCareListView.as_view(), name="user_care_list"),
     path("senior/update/<int:id>/", family_post_views.update_senior),
     path("senior/delete/<int:id>/", family_post_views.delete_senior, name='delete_senior'),
     path("senior/list/", family_list_views.list_senior),
