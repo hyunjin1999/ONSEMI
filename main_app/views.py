f84b09214b7010aa73275be1ffac91d0a1ac4f32
LeeYoonhyeok <yoonhyeok321@gmail.com>
2024-07-04 15:21:47 +0900

    feat: main_app > views.py 수정

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from management_app.models import Senior, Care
+from django.contrib.auth.decorators import login_required
+from auth_app.models import User
 
 #Create your views here.
 def index(request):
@@ -17,13 +20,44 @@ def volunteer(request):
 def terms(request):
     return render(request,'page/terms.html')
 
+@login_required
 def user_page(request):
     user_type = request.user.user_type
+    
     if user_type == 'FAMILY':
-        return render(request, 'management_app/user_senior_list.html')
+        user_id = request.user.id
+        seniors = Senior.objects.filter(user_id=user_id)
+        context = {
+            'seniors': seniors
+        }
+        return render(request, 'management_app/user_senior_list.html', context)
+     
     elif user_type == 'VOLUNTEER':
-        return render(request, 'management_app/volunteer_care_list.html')
+        sort_by = request.GET.get("sort_by", "datetime")
+        order = request.GET.get("order", "asc")
+        user_id = request.GET.get("user", "")
+
+        if order == "desc":
+            sort_by = "-" + sort_by
+
+        cares = Care.objects.all()
+
+        if user_id:
+            cares = cares.filter(user_id=user_id)
+
+        cares = cares.order_by(sort_by)
+        users = User.objects.all()
+
+        context = {
+            "cares": cares,
+            "users": users,
+            "selected_user": user_id,
+        }
+
+        return render(request, "management_app/volunteer_care_list.html", context)        
+    
     elif user_type == 'ADMIN':
         return render(request, 'admin_dashboard')
+    
     return render(request, 'default_dashboard') 
 
