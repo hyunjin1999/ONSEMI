a1dffcffaf31048c5714f56b2977bd263bbf140a
chericode <heo2922@naver.com>
2024-07-05 09:57:27 +0900

    feat : 결제시 하나의 SHOP care 요청만 전송되게 수정

--- orders_app/signals.py
+++ orders_app/signals.py
@@ -3,11 +3,10 @@ from django.dispatch import receiver
 from orders_app.models import Order
 from management_app.models import Care
 
-# 주문이 접수가 되면 바로 SHOP 서비스를 요청한 셈이니까 이를 고려하여 작성
 @receiver(post_save, sender=Order)
 def create_shop_care(sender, instance, created, **kwargs):
     if created:
-        user = instance.user  # 주문을 생성한 사용자
+        user = instance.user
         Care.objects.create(
             care_type='SHOP',
             title=f'SHOP 서비스 요청 - {instance.id}',
--- payment_app/views.py
+++ payment_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from orders_app.models import Order, OrderItem
 from shop_app.models import Product
 from django.contrib import messages
+from management_app.models import Care
 
 def payment_form(request, order_id):
     order = get_object_or_404(Order, id=order_id)
@@ -18,7 +19,6 @@ def payment_success(request, order_id):
         product = item.product
         if product.stock >= item.quantity:
             product.stock -= item.quantity
-            order.paid = True
             product.save()
         else:
             messages.error(request, f'{product.name}의 재고가 부족합니다.')
@@ -28,31 +28,20 @@ def payment_success(request, order_id):
     order.paid = True
     order.save()
 
+    # 주문이 완료되면 care 객체를 생성
+    content = "\n".join([f"{item.quantity}x {item.product.name}" for item in order.items.all()])
+    
+    care, created = Care.objects.update_or_create(
+        care_type="SHOP",
+        user_id=order.user,
+        title=f'SHOP 서비스 요청 - {order.id}',
+        defaults={
+            'content': f'주문 번호 {order.id}에 대한 SHOP 서비스 요청입니다.\n\n{content}',
+            'care_state': 'NOT_APPROVED',
+        }
+    )
+
     return render(request, 'orders/order/created.html', {'order': order})
 
 def payment_fail(request, order_id):
     return render(request, 'payment_app/payment_fail.html', {'order_id': order_id})
-
-# from django.shortcuts import render, get_object_or_404
-# from orders_app.models import Order
-
-# def payment_form(request, order_id):
-#     order = get_object_or_404(Order, id=order_id)
-#     context = {
-#         'order': order,
-#     }
-#     return render(request, 'payment_app/payment_form.html', context)
-
-# def payment_success(request, order_id):
-#     order = get_object_or_404(Order, id=order_id)
-
-#     # 주문된 상품들의 재고를 줄입니다.
-#     for item in order.items.all():
-#         product = item.product
-#         product.stock -= item.quantity
-#         product.save()
-        
-#     return render(request, 'orders/order/created.html', {'order': order})
-
-# def payment_fail(request, order_id):
-#     return render(request, 'payment_app/payment_fail.html', {'order_id': order_id})
