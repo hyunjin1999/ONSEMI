5bcb09ac6a4a4a43ededdb5e94bf1b1faf1cfc68
chericode <heo2922@naver.com>
2024-07-05 14:11:55 +0900

    feat : autoslug 기능 구현

---
+++ shop_app/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Category, Product
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
--- shop_app/models.py
+++ shop_app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.urls import reverse
+from autoslug import AutoSlugField
+from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200,
-                            unique=True)
+    slug = AutoSlugField(populate_from='name', unique=True, always_update=True)
 
     class Meta:
         ordering = ['name']
@@ -27,11 +28,10 @@ class Product(models.Model):
                                  related_name='products',
                                  on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
+    slug = AutoSlugField(populate_from='name', unique=True, always_update=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10,
-                                decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=0)  # 소수점 없이 원화로 표시
     stock = models.PositiveIntegerField(default=0)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -44,7 +44,7 @@ class Product(models.Model):
             models.Index(fields=['name']),
             models.Index(fields=['-created']),
         ]
-
+    
     def __str__(self):
         return self.name
 
