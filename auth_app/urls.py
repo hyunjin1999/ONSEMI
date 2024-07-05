0af75a86ea4ba5940fe0cb5ac7cf4a6b41a5bb44
chericode <heo2922@naver.com>
2024-07-05 16:53:50 +0900

    feat : 이메일 기반으로 아이디 찾기, 비밀번호 찾기 기능 추가

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import auth_views, profile_update_views, profile_views
+from .views import auth_views, profile_update_views, profile_views, find_user_id_views
 
 urlpatterns = [
     path("login/", auth_views.login_user),
@@ -9,4 +9,6 @@ urlpatterns = [
     path("profile/", profile_views.show_user_profile),
     path("profile/update/", profile_update_views.update_profile),
     path("profile/update/password/", profile_update_views.update_password),
+    path('find_user_id/', find_user_id_views.find_user_id, name='find_user_id'),
+    path('reset_password/', auth_views.reset_password, name='reset_password'),
 ]
