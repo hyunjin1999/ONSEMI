728c39e26710ed455b4789751c52dd6a1d3e3d0f
chericode <heo2922@naver.com>
2024-07-08 15:33:33 +0900

    feat : 자세히 보기에서 필터링 기능 추가 완료

--- monitoring_app/urls.py
+++ monitoring_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import order_report_views, monitor_views, diagnosis_views
+from .views import order_report_views, monitor_views, diagnosis_views, download_csv_views, csv_view
 
 app_name = 'monitoring_app'
 
 urlpatterns = [
     path('generate/', order_report_views.generate, name='generate'),
-    path('download_order_csv/', order_report_views.download_order_csv, name='download_order_csv'),
-    path('download_care_csv/', order_report_views.download_care_csv, name='download_care_csv'),
-    path('csv_view/', order_report_views.csv_view, name='csv_view'),
+    path('download_order_csv/', download_csv_views.download_order_csv, name='download_order_csv'),
+    path('download_care_csv/', download_csv_views.download_care_csv, name='download_care_csv'),
+    path('csv_view/', csv_view.csv_view, name='csv_view'),
     path('family_monitor/', monitor_views.family_monitor, name='family_monitor'),
     path('diagnosis/<int:care_id>/', diagnosis_views.diagnosis_view, name='diagnosis'),
 ]
