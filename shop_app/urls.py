04c5264f454d0d2f1e8a6b842cf27284649146c4
chericode <heo2922@naver.com>
2024-07-08 16:41:22 +0900

    feat : 최근 본 상품 기능 구현

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -7,4 +7,17 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('add_to_recent/<int:id>/', views.add_to_recent_products, name='add_to_recent_products'),
+    path('remove_from_recent/<int:id>/', views.remove_from_recent_products, name='remove_from_recent_products'),
 ]
+
+# from django.urls import path
+# from . import views
+
+# app_name = 'shop_app'
+
+# urlpatterns = [
+#     path('', views.product_list, name='product_list'),
+#     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+#     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+# ]
