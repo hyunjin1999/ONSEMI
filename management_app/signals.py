666ccad8253e5f3c7b7bda64750c27713ec297d6
chericode <heo2922@naver.com>
2024-07-09 10:07:10 +0900

    fix : management_app signals.py 수정

--- management_app/signals.py
+++ management_app/signals.py
@@ -17,16 +17,3 @@ def update_care_status_on_report_delete(sender, instance, **kwargs):
     care = instance.care
     care.care_state = 'APPROVED'
     care.save()
-
-# 주문이 접수가 되면 바로 SHOP 서비스를 요청한 셈이니까 이를 고려하여 작성
-@receiver(post_save, sender=Order)
-def create_shop_care(sender, instance, created, **kwargs):
-    if created:
-        user = instance.user  # 주문을 생성한 사용자
-        Care.objects.create(
-            care_type='SHOP',
-            title=f'SHOP 서비스 요청 - {instance.id}',
-            content=f'주문 번호 {instance.id}에 대한 SHOP 서비스 요청입니다.',
-            user_id=user,
-            care_state='NOT_APPROVED',
-        )
