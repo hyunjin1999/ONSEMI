61e09ac0120a4a7178967f02ce19029ad590cc6f
davin <sdabins2@gmail.com>
2024-07-12 17:23:10 +0900

    feat: 날짜 정렬 추가

--- monitoring_app/views/monitor_report_list_views.py
+++ monitoring_app/views/monitor_report_list_views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render,get_object_or_404
 from management_app.models import Care, Senior, Report, ReportImage
+from ..forms import FilterForm
+from django.core.paginator import Paginator
+
 
 def report_list(request):
     sort_by = request.GET.get('sort_by', '-created_at')
+    form = FilterForm(request.GET)
     user_id = request.user.id # 현재 로그인한 사용자의 ID
     selected_senior_id = request.GET.get('selected_senior_id', '')
     type_filter = request.GET.get('type_filter', 'all')
    
-    reports = Report.objects.all().order_by(sort_by)
     # 현재 로그인한 사용자의 Care 객체만 필터링
     cares = Care.objects.filter(user_id=user_id)
     if selected_senior_id:
@@ -21,10 +24,12 @@ def report_list(request):
     if selected_senior_id:
         selected_senior = Senior.objects.get(id=selected_senior_id)
 
-    if sort_by == '-created_at':
-        reports = reports.order_by('-created_at')
-    else:
-        reports = reports.order_by('created_at')
+    if form.is_valid():
+        start_date = form.cleaned_data.get('start_date')
+        end_date = form.cleaned_data.get('end_date')
+        
+        if start_date and end_date:
+            reports = reports.filter(created_at__date__gte=start_date, created_at__date__lte=end_date)
         
     if type_filter == 'shop':
         reports = reports.filter(care__care_type='쇼핑')
@@ -34,6 +39,11 @@ def report_list(request):
         reports = reports.filter(care__care_type='병원')   
         
     
+    if sort_by == '-created_at':
+        reports = reports.order_by('-created_at')
+    else:
+        reports = reports.order_by('created_at')
+        
     reports_imgs = ReportImage.objects.filter(report__in=reports)
     
     context = {
@@ -43,6 +53,7 @@ def report_list(request):
         "seniors": seniors,
         "selected_senior": selected_senior,
         "type_filter": type_filter,
+        'form': form,
         "reports" : reports,
         "report_imgs" : reports_imgs,
     }
